89e1394b7ba65b85f5af38f2d9fb3137cab3e49a
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2025-01-11 10:25:43 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -37,3 +37,7 @@ module "rds" {
   publicly_accessible         = var.publicly_accessible
   skip_final_snapshot         = var.skip_final_snapshot
 }
+module "cloudfront" {
+  source      = "/home/ec2-user/terraform/module/cloudfront"
+  bucket_name = var.bucket_name  
+ 
