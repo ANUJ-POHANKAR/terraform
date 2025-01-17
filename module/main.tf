c1b1928db200aa86df06510d50fec6209b81a397
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2025-01-17 21:28:49 +0530

    frontend

--- module/main.tf
+++ module/main.tf
@@ -22,23 +22,23 @@ terraform {
   instance_types  = var.instance_types
   node_role_arn     = var.node_role_arn
   cluster_role_arn  = var.cluster_role_arn
- }
-## module "rds" {
-## source                      = "/home/ec2-user/terraform/module/rds"
-## allocated_storage           = var.allocated_storage
-## db_name                     = var.db_name
-##  engine                      = var.engine
-## engine_version              = var.engine_version
-##  vpc_id                      = var.vpc_id
-##  instance_class              = var.instance_class
-##  master_password             = var.master_password
-##  username                    = var.username
-##  parameter_group_name        = var.parameter_group_name
-##  publicly_accessible         = var.publicly_accessible
-##  skip_final_snapshot         = var.skip_final_snapshot
-## }
-## module "cloudfront" {
-##  source      = "/home/ec2-user/terraform/module/cloudfront"
-##  bucket_name = var.bucket_name  
-## }
+}
+module "rds" {
+  source                      = "/home/ec2-user/terraform/module/rds"
+  allocated_storage           = var.allocated_storage
+  db_name                     = var.db_name
+  engine                      = var.engine
+  engine_version              = var.engine_version
+  vpc_id                      = var.vpc_id
+  instance_class              = var.instance_class
+  master_password             = var.master_password
+  username                    = var.username
+  parameter_group_name        = var.parameter_group_name
+  publicly_accessible         = var.publicly_accessible
+  skip_final_snapshot         = var.skip_final_snapshot
+}
 
+module "cloudfront" {
+  source      = "/home/ec2-user/terraform/module/cloudfront"
+  bucket_name = var.bucket_name  # Corrected this line
+}
